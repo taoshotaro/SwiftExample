1e791870e1ada2d49960fc32955df42ef125c7d9
taoshotaro <tao@shotr.me>
2021-04-30 16:09:56 +0900

    first commit

---
+++ StoryboardUIKitAppDelegate/Sources/Components/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  StoryboardUIKitAppDelegate
+//
+//  Created by Shotaro Tao on 2021/04/30.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+
+        // Do any additional setup after loading the view.
+    }
+}
